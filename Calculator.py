cde3b83ca82f2613d3a23fcfddc7540fac9fbea2
Autumn <34965675+Autumn808@users.noreply.github.com>
2020-03-15 11:32:28 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,85 +1,105 @@
 """Autumn Capasso
 UMUC SDEV 300
-Oct 23, 2019
-Python Calculator Program
+March 15, 2020
+Lab 1 Python Calculator Program
 This program can add, subtract, multiply, divide, and modulos
 """
-
-# Display Menu
-
-print('Welcome to the Calculator Program ')
-print('Select operation')
-print('1. Add')
-print('2. Subtract')
-print('3. Multiply')
-print('4. Divide')
-print('5. Modulus ')
+import sys
 
 
-# Functions for Calculator
+def main():
+    # Display Menu
+    print('Welcome to the Calculator Program ')
+    print('Select operation')
+    print('1. Add')
+    print('2. Subtract')
+    print('3. Multiply')
+    print('4. Divide')
+    print('5. Modulus ')
 
-# Addition Function
-def add(x, y):
-    return x + y
+    # Functions for Calculator
+
+    # Addition Function
+
+
+""" Will compute the addition of two values passed into the function  """
+
+
+def add(x_value, y_value):
+    return x_value + y_value
 
 
 # Subtraction Function
-def sub(x, y):
-    return x - y
+""" Will compute the subtraction of two values passed into the function  """
+
+
+def sub(x_value, y_value):
+    return x_value - y_value
 
 
 # Multiplication Function
-def multiply(x, y):
-    return x * y
+""" Will compute the multiplication of two values passed into the function  """
+
+
+def multiply(x_value, y_value):
+    return x_value * y_value
 
 
 # Divide Function
-def divide(x, y):
-    return x / y
+""" Will compute the division of two values passed into the function  """
+
+
+def divide(x_value, y_value):
+    return x_value / y_value
 
 
 # Modulus Function
-def modulus(x, y):
-    return x % y
+""" Will compute the modulus of two values passed into the function  """
+
+
+def modulus(x_value, y_value):
+    return x_value % y_value
 
 
 # Exit Function
 def end_program():
-    exit()
+    sys.exit()
 
 
 # User Input for Selection
+SELECTION: int = int(input("Enter Selection: "))
 
-selection = input("Enter selection: ")
-
-if selection >= '6':
+if SELECTION >= 6:
     print('Invalid input')
     print('Thank you for trying the calculator program', end_program())
 
-Num1 = int(input("Enter first number: "))
-Num2 = int(input("Enter second number: "))
+NUM1 = int(input("Enter first number: "))
+NUM2 = int(input("Enter second number: "))
 
-if selection == '1':
-    print(Num1, "+", Num2, "=", add(Num1, Num2))
+if SELECTION == 1:
+    print(NUM1, "+", NUM2, "=", add(NUM1, NUM2))
     print('Thank you for trying the calculator program')
 
-if selection == '2':
-    print(Num1, "-", Num2, "=", sub(Num1, Num2))
+if SELECTION == 2:
+    print(NUM1, "-", NUM2, "=", sub(NUM1, NUM2))
     print('Thank you for trying the calculator program')
 
-if selection == '3':
-    print(Num1, "*", Num2, "=", multiply(Num1, Num2))
+if SELECTION == 3:
+    print(NUM1, "*", NUM2, "=", multiply(NUM1, NUM2))
     print('Thank you for trying the  calculator program')
 
-if selection == '4':
+if SELECTION == 4:
 
-    if Num2 != 0:
-        print(Num1, "/", Num2, "=", divide(Num1, Num2))
+    if NUM2 != 0:
+        print(NUM1, "/", NUM2, "=", divide(NUM1, NUM2))
         print('Thank you trying the calculator program')
 
-    if Num2 == 0:
+    if NUM2 == 0:
         print("Invalid input")
 
-if selection == '5':
-    print(Num1, "%", Num2, "=", modulus(Num1, Num2))
+if SELECTION == 5:
+    print(NUM1, "%", NUM2, "=", modulus(NUM1, NUM2))
     print('Thank you for trying to calculator program')
+
+if __name__ == "__main__":
+    main()
